388602c2f53d634122fb00214a9cf966089fcc0f
Filip <olbromski.filip@gmail.com>
2022-03-20 10:08:07 +0100

    Setting up env vars

--- olbromski_site/settings.py
+++ olbromski_site/settings.py
@@ -12,22 +12,29 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from pickle import FALSE
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j4smj6q460t8#0gi!60b%)-wmh=7kd5m#r0c*6c#7-61$uat1l'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+  
+]
 
 # Application definition
 
